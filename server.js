d98d5c5db350687a525e797d6a0d79cfb9a85139
bisnu-ai <sambunu2003@gamil.com>
2026-02-17 21:24:23 +0530

    cors preflightcontinue false

--- server.js
+++ server.js
@@ -22,8 +22,9 @@ app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 }));
-app.options("*", cors());
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
